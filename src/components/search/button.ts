ed6804de324068717c11b808f3d34b53edd4211f
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-11-11 19:23:43 +0300

    Create search/button component

---
+++ src/components/search/button.ts
@@ -0,0 +1,9 @@
+import '../../../assets/styles/button.css';
+
+const button = document.createElement('button');
+
+button.type = 'submit';
+button.className = 'button';
+button.textContent = 'Search';
+
+export default button;
