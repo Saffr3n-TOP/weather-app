09c1e424ed099a37b9ccb12624a9a2326d24b3fe
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-11-11 17:36:48 +0300

    Create normalizeWeatherData utility

---
+++ src/utils/normalizeWeatherData.ts
@@ -0,0 +1,23 @@
+import WeatherData from '../types/weatherData';
+import WeatherDataNormalized from '../types/weatherDataNormalized';
+
+export default function normalizeWeatherData(data: WeatherData) {
+  return {
+    location: {
+      country: data.location.country,
+      city: data.location.name
+    },
+    current: {
+      condition: {
+        icon: `http:${data.current.condition.icon}`,
+        description: data.current.condition.text
+      },
+      temperature: {
+        actualC: data.current.temp_c,
+        actualF: data.current.temp_f,
+        feelsLikeC: data.current.feelslike_c,
+        feelsLikeF: data.current.feelslike_f
+      }
+    }
+  } as WeatherDataNormalized;
+}
