e85ab101dc0cfdcd812077f66d12920a8ba1a5ae
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-11-11 19:24:14 +0300

    Create search/form component

---
+++ src/components/search/form.ts
@@ -0,0 +1,34 @@
+import '../../../assets/styles/form.css';
+import getWeatherData from '../../utils/getWeatherData';
+import normalizeWeatherData from '../../utils/normalizeWeatherData';
+import input from './input';
+import button from './button';
+
+const form = document.createElement('form');
+
+form.noValidate = true;
+form.className = 'form';
+form.append(input, button);
+
+form.onsubmit = async (e) => {
+  e.preventDefault();
+  input.value = input.value.trim();
+
+  if (!form.checkValidity()) {
+    input.setCustomValidity('Please fill out this field.');
+    return form.reportValidity();
+  }
+
+  const location = input.value;
+  const weatherData = await getWeatherData(location);
+
+  if (weatherData instanceof Error) {
+    input.setCustomValidity(weatherData.message);
+    return form.reportValidity();
+  }
+
+  const normalizedData = normalizeWeatherData(weatherData);
+  console.log(normalizedData);
+};
+
+export default form;
