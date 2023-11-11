16cbc7550583a756b288f2819cb0a4a25848fa05
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-11-11 20:06:32 +0300

    Create weather/image component

---
+++ src/components/weather/image.ts
@@ -0,0 +1,10 @@
+import '../../../assets/styles/image.css';
+
+const image = document.createElement('img');
+
+image.src = 'http://cdn.weatherapi.com/weather/64x64/night/113.png';
+image.alt = '';
+image.className = 'image';
+image.ariaHidden = 'true';
+
+export default image;
