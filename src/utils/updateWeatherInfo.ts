1b7b429ee598754e945e035a039f1d43545c7e5a
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-11-12 13:05:27 +0300

    Combine update functions in one utility

---
+++ src/utils/updateWeatherInfo.ts
@@ -0,0 +1,13 @@
+import { updateLocation } from '../components/weather/location';
+import { updateCondition } from '../components/weather/condition';
+import { updateImage } from '../components/weather/image';
+import { updateActualTemp } from '../components/weather/actualTemp';
+import { updateFeelTemp } from '../components/weather/feelTemp';
+
+export default {
+  updateLocation,
+  updateCondition,
+  updateImage,
+  updateActualTemp,
+  updateFeelTemp
+};
