b95553693d69dacda683e29ec7f066f45fd66f47
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-11-12 13:44:13 +0300

    Use default weather data on load

---
+++ src/components/error.ts
@@ -0,0 +1,5 @@
+const error = document.createElement('span');
+error.className = 'text text_large text_bold';
+error.textContent = 'Server error. Please try again later.';
+
+export default error;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
 import '../assets/styles/style.css';
+import { getDefaultWeatherData } from './utils/getWeatherData';
+import normalizeWeatherData from './utils/normalizeWeatherData';
+import updateWeatherInfo from './utils/updateWeatherInfo';
 import content from './components/content';
+import error from './components/error';
 
 const root = document.querySelector('.root');
-root.appendChild(content);
+
+(async () => {
+  const weatherData = await getDefaultWeatherData();
+  if (weatherData instanceof Error) return root.appendChild(error);
+
+  const normalizedData = normalizeWeatherData(weatherData);
+  root.appendChild(content);
+
+  const {
+    updateLocation,
+    updateCondition,
+    updateImage,
+    updateActualTemp,
+    updateFeelTemp
+  } = updateWeatherInfo;
+
+  updateLocation(normalizedData.location.country, normalizedData.location.city);
+  updateCondition(normalizedData.current.condition.description);
+  updateImage(normalizedData.current.condition.icon);
+  updateActualTemp(
+    normalizedData.current.temperature.actualC,
+    normalizedData.current.temperature.actualF
+  );
+  updateFeelTemp(
+    normalizedData.current.temperature.feelsLikeC,
+    normalizedData.current.temperature.feelsLikeF
+  );
+})();
