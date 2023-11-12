091cf055f8688cbe32fb208436d5e2eb47886049
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-11-12 13:18:58 +0300

    Change error message on unsuccessful fetch

--- src/utils/getWeatherData.ts
+++ src/utils/getWeatherData.ts
@@ -2,11 +2,12 @@ import WeatherData from '../types/weatherData';
 import ErrorData from '../types/errorData';
 
 const API_KEY = '65ac46dfc9cc48478c8190749230811';
+const SERVER_ERR_MSG = 'Server error. Please try again later.';
 
 export default async function getWeatherData(location: string) {
   const response = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`
-  ).catch((err) => new Error(err));
+  ).catch(() => new Error(SERVER_ERR_MSG));
 
   if (response instanceof Error) {
     return response;
@@ -14,7 +15,7 @@ export default async function getWeatherData(location: string) {
 
   const data = await (
     response.json() as Promise<WeatherData | ErrorData>
-  ).catch((err) => new Error(err));
+  ).catch(() => new Error(SERVER_ERR_MSG));
 
   if ('error' in data) {
     return new Error(data.error.message);
